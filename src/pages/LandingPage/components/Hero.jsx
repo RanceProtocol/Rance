2273a0862ee8f7829d69c8e84d2e04f894086824
Aydesmond <avalonmsg@gmail.com>
2022-08-22 14:09:38 +0100

    update add image and space

--- src/pages/LandingPage/components/Hero.jsx
+++ src/pages/LandingPage/components/Hero.jsx
@@ -6,14 +6,14 @@ import CronoChainIcon from "../../../assets/cronosicon.png";
 import PolygonIcon from "../../../assets/Polygon.png";
 import CoinsZoom from "../../../assets/coinszoom.gif";
 
-const Hero = () => {
+function Hero() {
   return (
     <div
-      className="container flex flex-col-reverse items-center px-10 mx-auto space-y-0 md:px-20 md:py-20 md:space-y-0 md:flex-row"
+      className="container flex flex-col-reverse items-center px-10 mx-auto space-y-0 md:px-20 md:py-10 md:space-y-0 md:flex-row"
       id="hero"
     >
       {/* Left Content */}
-      <div className="flex flex-col mt-5 mb-32 space-y-5 md:w-1/2">
+      <div className="flex flex-col mt-5 mb-10 space-y-5 md:w-1/2">
         <h1 className="text-white max-w-lg text-3xl font-bold text-center md:leading-snug md:text-6xl md:text-left">
           Fastest & secure platform to insure your crypto
         </h1>
@@ -24,26 +24,28 @@ const Hero = () => {
           swing of their holdings.
         </p>
 
-        <div className="flex flex-col justify-center space-y-3 md:space-x-1 md:space-y-0 md:justify-start md:flex-row">
+        <div className="flex flex-col justify-center space-y-5 md:space-x-4 md:space-y-0 md:justify-start md:flex-row">
+          {/* eslint-disable-nextline */}
           <a
-            //eslint-disable-nextline
-            href="#"
-            className="flex items-center justify-around px-0 py-4 bg-primaryColor text-white rounded-full baseline md:justify-evenly md:px-7 md:space-x-1 md:mt-0"
+            href="#/"
+            className="flex items-center justify-around px-2 py-4 bg-primaryColor text-white rounded-full baseline md:justify-evenly md:px-7 md:space-x-1 md:mt-0"
           >
             Insure on Cronos Chain&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{" "}
             <img
               src={CronoChainIcon}
               className="bg-white text-primaryColor rounded-full ml-5 text-lg"
               style={{ fontSize: "30px" }}
+              alt=""
             />
           </a>
 
           <a
-            href="#"
-            className="flex items-center justify-around px-0 py-4 bg-primaryColor text-white rounded-full baseline md:justify-evenly md:px-7 md:space-x-1 md:mt-0"
+            href="#/"
+            className="flex items-center justify-around px-2 py-4 bg-primaryColor text-white rounded-full baseline md:justify-evenly md:px-7 md:space-x-1 md:mt-0"
           >
             Insure on BNBChain &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp;&nbsp;{" "}
+            {/* eslint-disable-next-line jsx-a11y/alt-text */}
             <img
               src={BNBChainIcon}
               className="bg-white text-primaryColor rounded-full ml-5 text-lg"
@@ -51,38 +53,47 @@ const Hero = () => {
             />
           </a>
         </div>
-        <a
-          href="#"
-          className="flex items-center justify-around px-0 py-4 bg-primaryColor text-white rounded-full baseline md:justify-evenly md:px-7 md:space-x-1 md:mt-0"
-        >
-          Insure on Polygon
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{" "}
-          <img
-            src={PolygonIcon}
-            className="bg-white text-primaryColor rounded-full ml-5 text-lg"
-            style={{ fontSize: "30px" }}
-          />
-        </a>
-        <a
-          href="#"
-          className="flex items-center justify-between w-96 px-8 py-4 border-solid border-2 border-primaryColor text-white rounded-full baseline"
-        >
-          Get $RANCE{" "}
-          <MdOutlineKeyboardArrowRight
-            className="bg-primaryColor text-white rounded-full ml-5 text-lg md:ml-0"
-            style={{ fontSize: "30px" }}
-          />
-        </a>
+
+        <div className="flex flex-col justify-center space-y-5 md:space-x-4 md:space-y-0 md:justify-start md:flex-row">
+          {/* eslint-disable-nextline */}
+          <a
+            href="#/"
+            className="flex items-center justify-around px-2 py-4 bg-primaryColor text-white rounded-full baseline md:justify-evenly md:px-7 md:space-x-1 md:mt-0"
+          >
+            Insure on Polygon
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{" "}
+            <img
+              src={PolygonIcon}
+              className="bg-white text-primaryColor rounded-full ml-5 text-lg"
+              style={{ fontSize: "30px" }}
+              alt=""
+            />
+          </a>
+
+          <a
+            href="#/"
+            className="flex items-center justify-around px-2 py-4 border-solid border-2 border-primaryColor text-white rounded-full baseline md:justify-evenly md:px-7 md:space-x-1 md:mt-0"
+          >
+            Get $RANCE
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <MdOutlineKeyboardArrowRight
+              className="bg-primaryColor text-white rounded-full ml-5 text-lg md:ml-28"
+              style={{ fontSize: "30px" }}
+            />
+          </a>
+        </div>
+
         <div>
           <img src={CoinsZoom} alt="coin-zoom" />
         </div>
       </div>
+
       {/* Right Content -> Image */}
       <div>
         <img src={HeroGif} alt="coin-gif" className="zoom-in-zoom-out" />
       </div>
     </div>
   );
-};
+}
 
 export default Hero;
